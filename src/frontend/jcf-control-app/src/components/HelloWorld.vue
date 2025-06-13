100e82d39551a3565f76edc700927fb232e7e49e
Joao Carias de Franca <joaocariasdefranca@gmail.com>
2025-06-12 23:43:31 -0300

    iniciando projeto frontend

---
+++ src/frontend/jcf-control-app/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline center">    Hello world!  </h1>
+
+  <div class="flex flex-row">  <div class="basis-3xs">01</div>  <div class="basis-2xs">02</div>  <div class="basis-xs">03</div>  <div class="basis-sm">04</div></div>
+</template>
+
