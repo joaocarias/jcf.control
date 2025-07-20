ed574ffbd4fc2dc624a62d9472a1bcbe06d64823
Joao Carias de Franca <joaocariasdefranca@gmail.com>
2025-07-20 20:51:34 -0300

    inciniciando novo projeto em front

---
+++ src/frontend/jcf-control-my-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBarLadingPage from './components/navbar/NavBarLandingPage.vue'
+</script>
+
+<template>
+  <NavBarLadingPage />
+</template>
+
+<style scoped>
+
+</style>
